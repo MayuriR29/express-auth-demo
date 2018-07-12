747046b4b79518e7a9b2d399fd90a2eb688aaeef
davified <davified@gmail.com>
2018-07-12 11:37:20 +0800

    Remove unused import

--- routes/secretsRouter.js
+++ routes/secretsRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const passport = require("passport");
 
 router.get("/", (req, res) => {
   res.json({ message: "Success! You can not see this without a token" });
